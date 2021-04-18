547c106fdf8d7b6bd352ace9bef2d7c8e0cabc10
melnelen <melnelen@gmail.com>
2021-04-18 19:38:58 +0200

    WIP drag and drop of the puzzle pieces

--- Gridy/Controller/PuzzleViewController.swift
+++ Gridy/Controller/PuzzleViewController.swift
@@ -12,9 +12,19 @@ class PuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
     
     var imagePieces: [UIImage]!
     var imageEditor: ImageEditorViewController!
+    var origin: CGRect!
+    var initialImageViewOffset = CGPoint()
+    var translation: CGPoint = .zero
     
-    @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet var puzzlePiecesImageViews: [UIImageView]!
+    @IBOutlet var puzzlePiecesPlaceholdersViews: [UIView]!
+    @IBOutlet var puzzleBlocksViews: [UIView]!
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var hintImageView: UIImageView!
+    @IBOutlet weak var soundImageView: UIImageView!
+    @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet weak var movesNimberLabel: UILabel!
+    
     
     
     override func viewDidLoad() {
@@ -22,6 +32,7 @@ class PuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
         
         setupNewGameButton()
         setupPuzzlePiecesImageViews()
+        configureGestures(view: self.puzzlePiecesImageViews[0]) //I don't know how to configure gestures for an array of elements :(
     }
     
     private func setupNewGameButton() {
@@ -35,22 +46,90 @@ class PuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
         self.newGameButton.clipsToBounds = true
     }
     
+    private func setupPuzzlePiecesImageViews() {
+        let randomizedImagePieces = imagePieces.shuffled()
+        
+        //I want to apply to the puzzle pieces the same frames as the pieces placeholders
+        for puzzlePiece in puzzlePiecesImageViews {
+            for puzzlePiecePlaceholder in puzzlePiecesPlaceholdersViews {
+                puzzlePiece.frame = puzzlePiecePlaceholder.frame
+            }
+        }
+        
+        for (index, puzzlePiece) in puzzlePiecesImageViews.enumerated() {
+            //fill puzzle pieces with images
+            puzzlePiece.image = randomizedImagePieces[index]
+        }
+    }
+    
+    private func setupPuzzlePiecesPlaceholdersViews() {
+        for puzzlePiecePlaceholder in puzzlePiecesPlaceholdersViews {
+            //setup image views borders
+            puzzlePiecePlaceholder.layer.borderColor = UIColor(named: Constant.Color.primaryLight)?.cgColor
+            puzzlePiecePlaceholder.layer.masksToBounds = true
+            puzzlePiecePlaceholder.contentMode = .scaleToFill
+            puzzlePiecePlaceholder.layer.borderWidth = 1
+        }
+    }
+    
     @IBAction func startNewGame(_ sender: Any) {
         dismiss(animated: true)
     }
     
-    private func setupPuzzlePiecesImageViews() {
-        let randomizedImagePieces = imagePieces.shuffled()
+    func configureGestures(view: UIView) {
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(selectPuzzlePieceImageView(_:)))
+        longPressGestureRecognizer.delegate = self
+        view.addGestureRecognizer(longPressGestureRecognizer)
         
-        for (index, puzzlePiece) in puzzlePiecesImageViews.enumerated() {
-            //setup image views borders
-            puzzlePiece.layer.borderColor = UIColor(named: Constant.Color.primaryLight)?.cgColor
-            puzzlePiece.layer.masksToBounds = true
-            puzzlePiece.contentMode = .scaleToFill
-            puzzlePiece.layer.borderWidth = 1
-            //fill puzzle pieces with images
-            puzzlePiece.image = randomizedImagePieces[index]
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(movePuzzlePieceImageView(_:)))
+        panGestureRecognizer.delegate = self
+        view.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc func selectPuzzlePieceImageView(_ sender: UILongPressGestureRecognizer) {
+        origin = view.frame
+    }
+    
+    @objc func movePuzzlePieceImageView(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        
+        if sender.state == .began {
+            initialImageViewOffset = self.translation
         }
+        
+        if sender.state == .changed {
+            self.translation = CGPoint(
+                x: initialImageViewOffset.x + translation.x,
+                y: initialImageViewOffset.y + translation.y
+            )
+        }
+        
+        if sender.state == .ended {
+            if !self.view.frame.contains(self.view.frame) {
+                view.frame = origin
+            }
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
+    -> Bool {
+        // simultaneous gesture recognition will only be supported for a puzzlePiece
+        for puzzlePiece in puzzlePiecesImageViews {
+            if gestureRecognizer.view != puzzlePiece {
+                return false
+            }
+        }
+        
+        // neither of the recognized gestures should not be tap gesture
+        if gestureRecognizer is UITapGestureRecognizer
+            || otherGestureRecognizer is UITapGestureRecognizer
+            || gestureRecognizer is UIPanGestureRecognizer
+            || otherGestureRecognizer is UIPanGestureRecognizer {
+            return false
+        }
+        
+        return true
     }
     
 }
